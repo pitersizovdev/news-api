23002b6a4cfae8f0442b6758addfbcdacf43e4a2
pitersizovdev <pitersizov@gmail.com>
2024-01-23 20:19:06 +0300

    0.2.0 - styling

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -5,11 +5,7 @@ const Pagination = ({ totalPages, handleNextPage, handlePreviousPage, handlePage
   return (
     <div className={styles.pagination}>
       <button onClick={handlePreviousPage} className={styles.arrow}>{'<'}</button>
-      <div className={styles.list}>
-        {[...Array(totalPages)].map((_, index) => {
-          return <button disabled={index + 1 === currentPage} onClick={() => handlePageClick(index + 1)} className={styles.pageNumber} key={index}>{index + 1}</button>;
-        })}
-      </div>
+          <span className={styles.pageNumber}>{currentPage}</span>
       <button onClick={handleNextPage} className={styles.arrow}>{'>'}</button>
     </div>
   );
